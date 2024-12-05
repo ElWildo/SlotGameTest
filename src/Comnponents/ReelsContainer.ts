ca74c8cd30fb3bc3254bd1d608cc7b1270f42591
elWildo <francescowild90.com>
2024-12-05 21:09:02 +0000

    First Commit

---
+++ src/Comnponents/ReelsContainer.ts
@@ -0,0 +1,196 @@
+import {
+  Assets,
+  BlurFilter,
+  Container,
+  ContainerChild,
+  Sprite,
+  Texture,
+} from "pixi.js";
+
+export interface Reel {
+  container: Container<ContainerChild>;
+  symbols: Sprite[];
+  position: number;
+  previousPosition: number;
+  blur: BlurFilter;
+}
+
+export interface TwinTo {
+  object: Reel;
+  property: "position";
+  propertyBeginValue: Reel["position"];
+  target: number;
+  time: number;
+  easing: (n: number) => number;
+  complete: (() => void) | null;
+  start: number;
+}
+
+export const REEL_WIDTH = 260;
+export const SYMBOL_SIZE = 250;
+
+await Assets.load([
+  "./assets/symbols/s01.png",
+  "./assets/symbols/s02.png",
+  "./assets/symbols/s03.png",
+  "./assets/symbols/s04.png",
+  "./assets/symbols/s05.png",
+  "./assets/symbols/s06.png",
+  "./assets/symbols/s07.png",
+  "./assets/symbols/s08.png",
+  "./assets/symbols/s09.png",
+]);
+
+export const slotTextures = [
+  Texture.from("./assets/symbols/s01.png"),
+  Texture.from("./assets/symbols/s01.png"),
+  Texture.from("./assets/symbols/s02.png"),
+  Texture.from("./assets/symbols/s03.png"),
+  Texture.from("./assets/symbols/s04.png"),
+  Texture.from("./assets/symbols/s05.png"),
+  Texture.from("./assets/symbols/s06.png"),
+  Texture.from("./assets/symbols/s07.png"),
+  Texture.from("./assets/symbols/s08.png"),
+  Texture.from("./assets/symbols/s09.png"),
+];
+
+export default class ReelsContainer extends Container {
+  public reels: Reel[] = [];
+  public tweening: TwinTo[] = [];
+  private running = false;
+  constructor() {
+    super();
+    this.pivot.x = this.width / 2;
+    this.pivot.y = this.height / 2;
+    for (let i = 0; i < 5; i++) {
+      const rc = new Container();
+
+      rc.x = i * REEL_WIDTH;
+      this.addChild(rc);
+
+      const reel: Reel = {
+        container: rc,
+        symbols: [],
+        position: 0,
+        previousPosition: 0,
+        blur: new BlurFilter(),
+      };
+
+      reel.blur.blurX = 0;
+      reel.blur.blurY = 0;
+      rc.filters = [reel.blur];
+
+      // Build the symbols
+      for (let j = 0; j < 4; j++) {
+        const symbol = new Sprite(
+          slotTextures[Math.floor(Math.random() * slotTextures.length)]
+        );
+        // Scale the symbol to fit symbol area.
+
+        symbol.y = j * SYMBOL_SIZE;
+        symbol.scale.x = symbol.scale.y = Math.min(
+          SYMBOL_SIZE / symbol.width,
+          SYMBOL_SIZE / symbol.height
+        );
+        symbol.x = Math.round((SYMBOL_SIZE - symbol.width) / 2);
+        reel.symbols.push(symbol);
+        rc.addChild(symbol);
+      }
+      this.reels.push(reel);
+    }
+  }
+
+  tweenTo(
+    object: Reel,
+    property: "position",
+    target: number,
+    time: number,
+    easing: (n: number) => number,
+    oncomplete: (() => void) | null
+  ) {
+    const tween: TwinTo = {
+      object,
+      property,
+      propertyBeginValue: object[property],
+      target,
+      easing,
+      time,
+      complete: oncomplete,
+      start: Date.now(),
+    };
+
+    this.tweening.push(tween);
+
+    return tween;
+  }
+
+  // Function to start playing.
+  startPlay = () => {
+    if (this.running) return;
+    this.running = true;
+
+    for (let i = 0; i < this.reels.length; i++) {
+      const r = this.reels[i];
+      const target = r.position + 10 + i * 5;
+      const time = 2500 + i * 600;
+
+      this.tweenTo(
+        r,
+        "position",
+        target,
+        time,
+        this.backout(0.5),
+        i === this.reels.length - 1 ? this.reelsComplete : null
+      );
+    }
+  };
+
+  async results() {
+    const rows = this.reels[0].symbols
+      .map((symbol: Sprite) => symbol.position.y)
+      .sort((a, b) => a - b)
+      .slice(1);
+
+    for (let index = 0; index < rows.length; index++) {
+      let lastsymb: string | undefined = undefined;
+      let count = 0;
+      for (let i = 0; i < this.reels.length; i++) {
+        const symb = await this.reels[i].symbols.find(
+          (symbol) => Math.floor(symbol.position.y) == rows[index]
+        )?.texture.label;
+        if (!symb) console.log("symb undefined");
+        if (!lastsymb || lastsymb != symb) {
+          if (lastsymb != symb) {
+            this.isWin(count);
+          }
+          lastsymb = symb;
+          count = 1;
+        } else {
+          count++;
+          if (i === this.reels.length - 1) this.isWin(count);
+        }
+      }
+      lastsymb = undefined;
+    }
+  }
+
+  isWin(count: number) {
+    if (count >= 3) {
+      console.log("Big Win");
+    } else if (count >= 2) {
+      console.log("Win");
+    }
+  }
+
+  // Reels done handler.
+  reelsComplete = async () => {
+    await this.results();
+    this.running = false;
+  };
+
+  // Backout function from tweenjs.
+  // https://github.com/CreateJS/TweenJS/blob/master/src/tweenjs/Ease.js
+  backout(amount: number) {
+    return (t: number) => --t * t * ((amount + 1) * t + amount) + 1;
+  }
+}
---
+++ src/main.ts
@@ -0,0 +1,168 @@
+import {
+  Application,
+  Color,
+  Graphics,
+  Text,
+  TextStyle,
+  FillGradient,
+} from "pixi.js";
+import "./style.css";
+import ReelsContainer, {
+  REEL_WIDTH,
+  slotTextures,
+  SYMBOL_SIZE,
+  TwinTo,
+} from "./Comnponents/ReelsContainer";
+
+(async () => {
+  const app = new Application();
+  await app.init({
+    resizeTo: window,
+    resolution: window.innerWidth / window.innerHeight,
+  });
+
+  document.body.appendChild(app.canvas);
+  app.renderer.resize(window.innerWidth, window.innerHeight - 4);
+  window.addEventListener("resize", function () {
+    app.renderer.resize(window.innerWidth, window.innerHeight - 4);
+  });
+
+  // Build the reels
+  const reelsContainer = new ReelsContainer();
+
+  app.stage.addChild(reelsContainer);
+
+  // Build top & bottom covers and position reelContainer
+  const margin = (app.screen.height - SYMBOL_SIZE * 3) / 2;
+
+  reelsContainer.y = margin;
+  reelsContainer.x = Math.round(
+    (app.screen.width + reelsContainer.width) / 2 - REEL_WIDTH * 5
+  );
+  const top = new Graphics()
+    .rect(0, 0, app.screen.width, margin)
+    .fill({ color: 0x0 });
+  const bottom = new Graphics()
+    .rect(0, SYMBOL_SIZE * 3 + margin, app.screen.width, margin)
+    .fill({ color: 0x0 });
+
+  // Create gradient fill
+  const fill = new FillGradient(0, 0, 0, 36 * 1.7);
+
+  const colors = [0xffffff, 0x00ff99].map((color) =>
+    Color.shared.setValue(color).toNumber()
+  );
+
+  colors.forEach((number, index) => {
+    const ratio = index / colors.length;
+
+    fill.addColorStop(ratio, number);
+  });
+
+  // Add play text
+  const style = new TextStyle({
+    fontFamily: "Arial",
+    fontSize: 36,
+    fontStyle: "italic",
+    fontWeight: "bold",
+    fill: { fill },
+    stroke: { color: 0x4a1850, width: 5 },
+    dropShadow: {
+      color: 0x000000,
+      angle: Math.PI / 6,
+      blur: 4,
+      distance: 6,
+    },
+    wordWrap: true,
+    wordWrapWidth: 440,
+  });
+
+  const playText = new Text({ text: "Spin the wheels!", style });
+
+  playText.x = Math.round((bottom.width - playText.width) / 2);
+  playText.y =
+    app.screen.height - margin + Math.round((margin - playText.height) / 2);
+  bottom.addChild(playText);
+
+  // Add header text
+  const headerText = new Text({ text: "FUN SLOT GAME", style });
+
+  headerText.x = Math.round((top.width - headerText.width) / 2);
+  headerText.y = Math.round((margin - headerText.height) / 2);
+  top.addChild(headerText);
+
+  app.stage.addChild(top);
+  app.stage.addChild(bottom);
+
+  // Set the interactivity.
+  bottom.eventMode = "static";
+  bottom.cursor = "pointer";
+  bottom.addListener("pointerdown", () => {
+    reelsContainer.startPlay();
+  });
+
+  // Listen for animate update.
+  app.ticker.add(() => {
+    // Update the slots.
+    for (let i = 0; i < reelsContainer.reels.length; i++) {
+      const r = reelsContainer.reels[i];
+      // Update blur filter y amount based on speed.
+      // This would be better if calculated with time in mind also. Now blur depends on frame rate.
+
+      r.blur.blurY = (r.position - r.previousPosition) * 8;
+      r.previousPosition = r.position;
+
+      // Update symbol positions on reel.
+      for (let j = 0; j < r.symbols.length; j++) {
+        const s = r.symbols[j];
+        const prevy = s.y;
+
+        s.y = ((r.position + j) % r.symbols.length) * SYMBOL_SIZE - SYMBOL_SIZE;
+        if (s.y < 0 && prevy > SYMBOL_SIZE) {
+          // Detect going over and swap a texture.
+          // This should in proper product be determined from some logical reel.
+          s.texture =
+            slotTextures[Math.floor(Math.random() * slotTextures.length)];
+          s.scale.x = s.scale.y = Math.min(
+            SYMBOL_SIZE / s.texture.width,
+            SYMBOL_SIZE / s.texture.height
+          );
+          s.x = Math.round((SYMBOL_SIZE - s.width) / 2);
+        }
+      }
+    }
+  });
+
+  // Listen for animate update.
+  app.ticker.add(async () => {
+    const now = await Date.now();
+    const remove = [];
+
+    for (let i = 0; i < reelsContainer.tweening.length; i++) {
+      const t: TwinTo = reelsContainer.tweening[i];
+      const phase = Math.min(1, (now - t.start) / t.time);
+
+      t.object[t.property] = await lerp(
+        t.propertyBeginValue,
+        t.target,
+        await t.easing(phase)
+      );
+      if (phase === 1) {
+        t.object[t.property] = t.target;
+        if (t.complete) await t.complete();
+        remove.push(t);
+      }
+    }
+    for (let i = 0; i < remove.length; i++) {
+      reelsContainer.tweening.splice(
+        reelsContainer.tweening.indexOf(remove[i]),
+        1
+      );
+    }
+  });
+
+  // Basic lerp funtion.
+  function lerp(a1: number, a2: number, t: number) {
+    return a1 * (1 - t) + a2 * t;
+  }
+})();
